af7c0731a9db918976b65912ddb5b5a28efb59ff
sirapat27 <sirapat.ne@baac.or.th>
2016-05-16 13:20:50 +0700

    Add project files.

---
+++ TestGit/TestUploadGit.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestGit
+{
+    public class TestUploadGit
+    {
+        public static int GetNumber()
+        {
+            return 0;
+        }
+        public static int GetNumber2()
+        {
+            return 2;
+        }
+        public static int GetNumber3()
+        {
+            return 13;
+        }
+        public static int GetNumber4()
+        {
+            return 14;
+        }
+        public static int GetNumber6()
+        {
+            return 6;
+        }
+        public static int GetNumber5()
+        {
+            return 5;
+        }
+        public static int GetNumber7()
+        {
+            return 5;
+        }
+    }
+}
